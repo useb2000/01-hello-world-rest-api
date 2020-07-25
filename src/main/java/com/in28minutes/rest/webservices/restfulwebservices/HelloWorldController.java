abf08911866447bd0c5913abfc72ca0d0f5ae318
useb2000 <66538552+useb2000@users.noreply.github.com>
2020-07-25 00:49:07 -0400

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World. This is version 1";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
